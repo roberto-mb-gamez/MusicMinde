abcbdbb362b89d30bfb63e84b1d19641ea6c7264
RobertoGamez <RobertoGamez@DESKTOP-RCJPCO1>
2021-12-05 13:42:33 -0600

    Initial creation
    Showing class added
    
    Add location property
    
    Add NumberOfSeats property
    
    Add ShowTimes property
    
    Remove using directive

---
+++ MusicMudeApi/Showing.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MusicMudeApi
+{
+    public class Showing
+    {
+        public string Location { get; set; }
+        public int NumberOfSeats { get; set; }
+        public List<DateTime> ShowTimes { get; set; }
+    }
+}
